053ec5071577c126d03404689c8499babb5f21fb
Ricardo Supe <mnzioss@gmail.com>
2025-02-18 23:33:54 -0500

    act

--- src/app/contenido/contenido-routing.module.ts
+++ src/app/contenido/contenido-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { MascotaComponent } from '../usuarios/pages/mascota/mascota.component';
 
 const routes: Routes = [
   {
