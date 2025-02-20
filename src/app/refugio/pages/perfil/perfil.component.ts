e583b08f9fa75380c6a9599b4f85ab6cc457b5ff
Ricardo Supe <mnzioss@gmail.com>
2025-02-19 20:52:14 -0500

    Update perfil.component.ts

--- src/app/refugio/pages/perfil/perfil.component.ts
+++ src/app/refugio/pages/perfil/perfil.component.ts
@@ -22,12 +22,20 @@ export class PerfilComponent {
     @Inject(PLATFORM_ID) private platformId: Object,
     private window: WindowService
   ) {
-    effect(() => {
+    /* effect(() => {
       const refugio = this.refugio();
       if (refugio && refugio.latitud && refugio.longitud) {
         this.initMap(parseFloat(refugio.latitud), parseFloat(refugio.longitud));
       }
+    }); */
+    effect(() => {
+      const refugio = this.refugio();
+      if (refugio && refugio.latitud && refugio.longitud) {
+        console.log('Refugio data:', refugio); // Verifica los datos en la consola
+        this.initMap(parseFloat(refugio.latitud), parseFloat(refugio.longitud));
+      } 
     });
+    
   }
 
   public refugio = toSignal(
@@ -40,7 +48,7 @@ export class PerfilComponent {
 
   ngOnInit() {}
 
-  private async initMap(latitud: number, longitud: number) {
+  /* private async initMap(latitud: number, longitud: number) {
     const L = await import('leaflet');
 
     latitud = parseFloat(this.refugio()!.latitud);
@@ -71,7 +79,43 @@ export class PerfilComponent {
         '_blank'
       );
     });
-  }
+  } */
+
+    private async initMap(latitud: number, longitud: number) {
+      if (isPlatformBrowser(this.platformId)) { // Asegúrate de que el código solo se ejecute en el navegador
+        const L = await import('leaflet');
+        
+        // Asegúrate de que el DOM esté disponible
+        if (!this.map) {
+          this.map = L.map('map').setView([latitud, longitud], 13);
+          
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors',
+          }).addTo(this.map);
+      
+          const marker = L.marker([latitud, longitud], {
+            icon: L.icon({
+              iconUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+              shadowSize: [41, 41],
+            }),
+          }).addTo(this.map);
+      
+          marker.on('click', () => {
+            window.open(
+              `https://www.google.com/maps/dir/?api=1&destination=${latitud},${longitud}`,
+              '_blank'
+            );
+          });
+        }
+      }
+    }
+    
 
   getSafeMapUrl(mapaUrl: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(mapaUrl);
